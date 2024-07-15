dc382e2574781c5775fc121f12e957f52894b061
Joona Somerkivi <joona.somerkivi@nexigroup.com>
2024-07-15 14:21:10 +0300

    trying to find functioning wweb version

--- whatsapp-spotify.js
+++ whatsapp-spotify.js
@@ -15,7 +15,7 @@ const playlistId = "1RuXVd1wpfXxBlWsOSmGqX"
 let playlistContent = []
 const maxPlaylistLength = 100
 
-const wwebVersion = '2.3000.1013902813-alpha'
+const wwebVersion = '2.2411.2'
 /* History
 2.2413.51-beta
 2.2410.1
